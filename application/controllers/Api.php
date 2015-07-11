ec84f6cec961f0e5b5e1dd2e837ec3f6dda927ff
Jeremy G <me@jeremyg.se>
2015-07-12 01:04:43 +0200

    Started restructuring and cleanup pt 2

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -26,10 +26,10 @@ class Api extends CI_Controller {
 	}
 
 	/**
-		index - /api/getmeal controller
+		getmeal - /api/getmeal controller
 			
 			Accepts numeric meal $id as input, passing meal data to the view. defaults to false if no $id is given
-			Loads /api/getmeal view.
+			Loads /api/json view.
 	**/
 	public function getmeal($id = false) {
 		
@@ -37,12 +37,78 @@ class Api extends CI_Controller {
 			$this->_api_error(400, "Bad request"); // Exit with error code 400 Bad Request
 		} else { // Supplied $id is of acceptable format
 			$this->load->model('meal_model'); // Load Meal_model
-
-			// Unfinished
+			$meal = $this->meal_model->get_meal($id); // Get meal. 
 			
-			$this->load->view('api/getmeal', $data); // Pass $data to the view main/index
+			if ($meal != false) {
+				// Calculate the percentage
+				if($meal['meals_up'] == 0 && $meal['meals_views'] == 0) { // Stop division by 0 later on, 'meals_up' is number of clicks and 'meals_views' is number of views.
+					$percentage = 0; 
+				} else {
+					$percentage = round( ($meal['meals_up'] / ($meal['meals_views'] + $meal['meals_up'])) * 100 ); // Divide number of clicks with number of views and clicks. TODO: Discuss whether this is the best approach. 
+				}
+						
+				$data = array( // Initialize $data array
+					"data" => array(
+						"type" => "meal",
+						'id' => $meal['meals_id'],
+						'name' => $meal['meals_name'],
+						'clicks' => $meal['meals_up'],
+						'views' => $meal['meals_views'],
+						'percentage' => $percentage,
+						'link' => $meal['meals_link'],
+						'owner' => $meal['meals_owner'],
+						'ownerlink' => $meal['meals_ownerlink']
+					),
+					"meta" => array("total" => $this->meal_model->get_total_meals())
+				); 
+			
+			
+				$this->load->view('api/json', array("output" => $data)); // Pass $data to the view main/index
+			}
+			else {
+				$this->_api_error(404, "Not Found"); // Exit with error code 404 Not Found
+			}
 		}
 	}
-
-
+	
+	
+	/**
+		click - /api/click controller
+			
+			Accepts numeric meal $id as input, passing meal data to the view. defaults to false if no $id is given
+			Loads /api/json view.
+	**/
+	public function click($id) {
+		$this->load->model('meal_model'); // Load Meal_model
+		
+		if (!is_numeric($id)) { // Not numeric
+			$this->_api_error(400, "Bad Request"); // Exit with error code 400 Bad Request
+		}
+		if(isset($_COOKIE['meal_clicks'])) { // Check if cookie is set
+			$cookie_data = json_decode($_COOKIE['meal_clicks'], true); // Decode the cookie string into an array
+			
+			if (!in_array($id,$cookie_data)) { // Meal id is not in cookie array, proceed to bump clicks
+				
+				$this->meal_model->update_click($id); // Bump clicks
+				
+				$cookie_data[] = $id; // Add $id to cookie array
+				setcookie('meal_clicks', json_encode($cookie_data), time()+1800); // Set the cookie again, 30MIN expiry time
+		
+				$this->_api_error(200, "OK"); // Exit with error code 200 OK
+				
+			}
+			else {
+				// User has already viewed this meal.
+				$this->_api_error(403, "Forbidden"); // Exit with error code 403 Forbidden
+			}
+		} else { // No cookie has been set
+			
+			$this->meal_model->update_click($id); // Bump clicks
+			
+			$cookie_data = array($id); // Add $id to cookie array
+			setcookie('meal_clicks', json_encode($cookie_data), time()+1800); // Set the cookie for the first time, 30MIN expiry time
+	
+			$this->_api_error(200, "OK"); // Exit with error code 200 OK
+		}
+	}
 }
--- application/models/Meal_model.php
+++ application/models/Meal_model.php
@@ -11,6 +11,43 @@ class Meal_model extends CI_Model {
 	
 	
 	/**
+		_update_views - Adds 1 to the number of views of a meal row [PRIVATE]
+	
+			Requires clean $id parameter 
+	**/
+	private function _update_views($id) {
+		
+		if(isset($_COOKIE['meal_views'])) { // Check if cookie is set
+			$cookie_data = json_decode($_COOKIE['meal_views'], true); // Decode the cookie string into an array
+			
+			if (!in_array($id,$cookie_data)) { // Meal id is not in cookie array, proceed to bump views
+				$this->db->where('meals_id', $id);
+				$this->db->set('meals_views', 'meals_views +1', FALSE);
+				$this->db->update('meals');
+		
+				$cookie_data[] = $id; // Add $id to cookie array
+				setcookie('meal_views', json_encode($cookie_data), time()+1800); // Set the cookie again, 30MIN expiry time
+		
+				return true;
+			}
+			else {
+				// User has already viewed this meal.
+				return false; // fail silently
+			}
+		} else { // No cookie has been set
+			$this->db->where('meals_id', $id);
+			$this->db->set('meals_views', 'meals_views +1', FALSE);
+			$this->db->update('meals');
+	
+			$cookie_data = array($id); // Add $id to cookie array
+			setcookie('meal_views', json_encode($cookie_data), time()+1800); // Set the cookie for the first time, 30MIN expiry time
+	
+			return true;
+		}
+	}
+
+	
+	/**
 		get_meal - Get one approved row/meal from the database
 	
 			Returns array of meal data of approved meal with $id if $id is supplied. 
@@ -20,14 +57,40 @@ class Meal_model extends CI_Model {
 		if($id != false) { // $id has been set
 			$sql = "SELECT * FROM meals WHERE meals_approved = ? AND meals_id = ? LIMIT 1"; // Get all data of a approved meal of given $id
 			$query = $this->db->query($sql, array(1, $id));
+			
+			$this->_update_views($id); // Update number of views
 			return $query->row_array();
 	 	}
 		else { // $id has not been set
 			$query = $this->db->query("SELECT * FROM meals WHERE meals_approved = 1 ORDER BY RAND() LIMIT 1"); // Select a random row
+			if($query->num_rows() == 1) {
+				$return = $query->row_array();
+				
+				$this->_update_views($return['meals_id']); // Update number of views
+				return $return; // Return row only if it is the only row.
+			} else {
+				return false; // Return false if more than 1 or 0 rows are returned.
+			}
 			
-			return $query->row_array();
 		}
 	}
+	
+	
+	/**
+		update_click - Bump number of clicks of a meal
+	
+			Requires clean $id, returns true (--if bump is successful--)
+	**/
+	public function update_click($id) { // Get the total number
+		$this->db->where('meals_id', $id);
+		$this->db->set('meals_up', 'meals_up +1', FALSE);
+		$this->db->update('meals');
+		
+		// TODO: Error catching
+		
+		return true;
+	}
+	
 	
 	/**
 		get_total_meals - Number of approved meals in the database
