529342a8265e961f95bfb4e0a1b3bcc2f17a8694
Jeremy G <me@jeremyg.se>
2015-07-12 20:29:41 +0200

    Fix

--- application/controllers/All.php
+++ application/controllers/All.php
@@ -9,10 +9,17 @@ class All extends CI_Controller {
 			Loads /all/index view.
 	**/
 	public function index() { // Index controller.
+		
+		
 		$this->load->model('meal_model'); // Load Meal_model
 		$meals = $this->meal_model->get_all_meals(1); // Get all meals
 
 		$data = array('meals' => $meals);
+		
+		$this->load->model('category_model'); // Load Category_model
+		$categories = $this->category_model->get_categories(); // Get all cetegories
+		shuffle($categories); // Shuffle the returned rows
+		$data['categories'] = $categories;
 		
 		$this->load->view('all/index', $data); // Display list page.
 	}
